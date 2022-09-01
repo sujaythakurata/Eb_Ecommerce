4a40fd1c452928e04a3d0f5fc95e5271b792fcb6
Sujay Thakurta <sujay_thakurta@epam.com>
2022-09-01 14:02:57 +0530

    header component & product reducer

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,8 +1,22 @@
 
-export default function Main(){
+import { Outlet } from "react-router-dom";
+import Header from "../../components/header/Header";
+import ErrorComponent from "../ErrorComponent";
+import "./main.scss"
+
+const Main = ()=>{
   return (
-    <div>
-      main page
+    <div className="grid-container">
+      <section className="header">
+        <Header/>
+      </section>
+      <section className="main-content">
+        <ErrorComponent>
+          <Outlet/>
+        </ErrorComponent>
+      </section>
     </div>
-  )
+  );
 }
+
+export default Main;
